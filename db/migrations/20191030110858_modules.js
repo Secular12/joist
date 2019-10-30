64f72383c7cf0f57e7b9acb9f6d110750ccebf1f
Mark Dekin <mark.dekin@gmail.com>
2019-10-30 12:28:51 -0400

    fix(migrations): Make modules name primary key
    **DESCRIPTION**
    For the purposes of the permissions table, removed the id as a primary
    key column for modules table and instead made the name the primary key
    
    Issue #20

--- db/migrations/20191030110858_modules.js
+++ db/migrations/20191030110858_modules.js
@@ -1,7 +1,6 @@
 exports.up = async knex => {
   await knex.schema.createTable('modules', table => {
-    table.increments()
-    table.string('name').notNullable()
+    table.string('name').notNullable().primary()
   })
 }
 
