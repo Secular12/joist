c98bfd91e49aa40951c80b156d9ce2ceb415fbf9
Mark Dekin <mark.dekin@gmail.com>
2019-10-30 17:41:12 -0400

    feat(migrations): Add users roles pivot table
    Issue #22

---
+++ db/migrations/20191030173616_users_roles.js
@@ -0,0 +1,21 @@
+exports.up = async knex => {
+  await knex.schema.createTable('users_roles', table => {
+    table
+      .integer('role_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('roles')
+    table
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+    table.primary(['user_id', 'role_id'])
+  })
+}
+
+exports.down = async knex => {
+  await knex.schema.dropTable('users_roles')
+}
