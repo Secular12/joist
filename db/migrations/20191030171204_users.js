3f72db6ea2651e7cd497ac7d5fe5cb069c1f14e1
Mark Dekin <mark.dekin@gmail.com>
2019-10-30 17:36:55 -0400

    fix(migrations): remove role_id from users table for users_roles pivot
    Issue #22

--- db/migrations/20191030171204_users.js
+++ db/migrations/20191030171204_users.js
@@ -5,12 +5,6 @@ exports.up = async knex => {
     table.string('first_name').notNullable()
     table.string('last_name').notNullable()
     table.string('password').notNullable()
-    table
-      .integer('role_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('roles')
     table.string('username').notNullable().unique()
     table.timestamps(false, true)
     table.datetime('deleted_at')
