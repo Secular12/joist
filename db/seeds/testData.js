97e721ef16b5849032ec400f2ed29b020e252a7c
Mark Dekin <mark.dekin@gmail.com>
2019-11-02 12:32:16 -0400

    feat(seeds): Roles permissions seeder
    **DESCRIPTION**
    - Added seeds for roles permissions table
    - added seeder to test data seeder
    
    Issue #37

--- db/seeds/testData.js
+++ db/seeds/testData.js
@@ -1,4 +1,5 @@
 exports.seed = async knex => {
   await require('./seeders/RolesSeeder')(knex)
+  await require('./seeders/RolesPermissionsSeeder')(knex)
   await require('./seeders/UsersSeeder')(knex)
 }
