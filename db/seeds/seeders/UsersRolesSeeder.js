0401044d2516a90609ad232e43edb2f1226c4baa
Mark Dekin <mark.dekin@gmail.com>
2019-11-02 13:07:19 -0400

    feat(seeds): Users roles seeder
    **DESCRIPTION**
    - Added users roles pivot seeds
    - Added seeder to test data seeder
    
    Issue #39

---
+++ db/seeds/seeders/UsersRolesSeeder.js
@@ -0,0 +1,17 @@
+const runFactory = require('../../lib/runFactory')
+
+const usersRolesFactory = i => ({
+  role_id: 2,
+  user_id: i + 1
+})
+
+module.exports = async knex => {
+  await knex('users_roles')
+    .insert([
+      {
+        role_id: 1,
+        user_id: 1
+      },
+      ...runFactory(usersRolesFactory, 10)
+    ])
+}
