132cfb872543d440c09efeeb0cf2509b4ff3fccc
Mark Dekin <mark.dekin@gmail.com>
2019-11-03 22:52:38 -0500

    feat(AuthModule): Add me and tokenRefresh compositions

--- api/modules/AuthModule/index.js
+++ api/modules/AuthModule/index.js
@@ -1,5 +1,6 @@
 const ContextModule = require('../ContextModule')
 const { GraphQLModule } = require('@graphql-modules/core')
+const isAuthenticated = require('../../compositions/isAuthenticated')
 const notAuthenticated = require('../../compositions/notAuthenticated')
 const resolvers = require('./resolvers')
 const typeDefs = require('./schema.graphql')
@@ -10,6 +11,8 @@ module.exports = new GraphQLModule({
   typeDefs,
   resolvers,
   resolversComposition: {
-    'Query.login': [notAuthenticated()]
+    'Query.login': [notAuthenticated()],
+    'Query.me': [isAuthenticated()],
+    'Query.tokenRefresh': [notAuthenticated()]
   }
 })
