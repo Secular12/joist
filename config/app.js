62c81db727316f332e4029c5dd43a98d5a7c9d2b
Mark Dekin <mark.dekin@gmail.com>
2019-11-12 00:45:20 -0500

    feat(config): add environment in env variables

--- config/app.js
+++ config/app.js
@@ -1,6 +1,7 @@
 const env = require('../lib/env')
 
 module.exports = {
+  environment: env('APP_ENVIRONMENT', 'development'),
   graphiql: env('APP_GRAPHIQL', false),
   host: env('APP_HOST', '127.0.0.1'),
   path: env('APP_PATH', '/api'),
